cde208ffb4ea6770ade2401831398e0e42e2e306
jrosmari <jrosmari@student.42.fr>
2023-01-26 17:32:36 +0100

    printf structure

---
+++ printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrosmari <jrosmari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 17:11:51 by jrosmari          #+#    #+#             */
+/*   Updated: 2023/01/26 17:21:52 by jrosmari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
