9651d506b7444deb00943fa3a9d1fcf4f3df423c
jrosmari <jrosmari@student.42.fr>
2023-01-28 10:02:49 +0100

    var functions testing

--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,57 @@
 /*   By: jrosmari <jrosmari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 17:14:43 by jrosmari          #+#    #+#             */
-/*   Updated: 2023/01/27 15:23:57 by jrosmari         ###   ########.fr       */
+/*   Updated: 2023/01/27 16:57:06 by jrosmari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-int	ft_printf(const char *, ...)
+void	ft_putchar(char c)
 {
+	write(1, &c, 1);	
+}
 
-	//git test
+int	ft_printf(const char *str, ...)
+{
+	va_list	ap;
+	int	i;
 
-
+	i = 0;
+	va_start(ap, str);
+	while (str[i] != '\0')
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			if (str[i] == 'c')
+			{
+				ft_putchar(va_arg(ap, int));
+			}
+			else if (str[i] == '%')
+				ft_putchar('%');
+			else
+			{
+				i--;
+				ft_putchar(str[i]);
+			}
+		}
+		else	
+			ft_putchar(str[i]);			
+		i++;
+	}
+	ft_putchar('\n');	
+	va_end(ap);
 	return (0);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	ft_printf("**%c***%d*****%c*****%c*******%a***%c****%*********%c", 'H', 'e', 'l', 'l', 'o');
+	printf("\n");
+	printf("%% %                             choj",'a');
+	printf("\n");
+	return(0);
 }
--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: jrosmari <jrosmari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 17:11:51 by jrosmari          #+#    #+#             */
-/*   Updated: 2023/01/26 17:21:52 by jrosmari         ###   ########.fr       */
+/*   Updated: 2023/01/27 18:07:42 by jrosmari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,23 @@
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+
+typedef struct s_print                       
+{                        
+      va_list  args;    # arg to print out                    
+      int   wdt;        # width                
+      int   prc;        # precision              
+      int   zero;       # zero padding               
+      int   pnt;        # .     
+      int   dash;       # -               
+      int   tl;         # total_length (return value)              
+      int   sign;       # pos or neg number               
+      int   is_zero;    # is number zero                 
+      int   perc;       # %               
+      int   sp;         # space flag ' '            
+}    t_print;
 
 int	ft_printf(const char *, ...);
 
