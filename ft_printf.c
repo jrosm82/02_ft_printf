99e24b3bc7b26d6f139dfaac670a0a2552992460
jrosmari <jrosmari@student.42.fr>
2023-01-28 11:11:53 +0100

    char string print tests

--- ft_printf.c
+++ ft_printf.c
@@ -6,22 +6,76 @@
 /*   By: jrosmari <jrosmari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 17:14:43 by jrosmari          #+#    #+#             */
-/*   Updated: 2023/01/27 16:57:06 by jrosmari         ###   ########.fr       */
+/*   Updated: 2023/01/28 11:10:46 by jrosmari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-void	ft_putchar(char c)
+int	ft_putchar(char c)
 {
-	write(1, &c, 1);	
+	return (write(1, &c, 1));	
 }
+
+int	ft_putstr(char *s)
+{	
+	int	i;
+	int	cnt;
+
+	i = 0;
+	cnt = 0;
+	while (s[i] != '\0')
+	{
+		cnt += ft_putchar(s[i]);
+		i++;
+	}
+	return (cnt);
+}
+
+int	pnt_decide(char c, va_list ap)
+{
+	int	prn_cnt;
+
+
+	prn_cnt = 0;
+	if (c == 'c') // Prints a single character.
+		prn_cnt += ft_putchar(va_arg(ap, int));
+	else if (c == 's') // Prints a string (as defined by the common C convention).
+		prn_cnt += ft_putstr(va_arg(ap, char *));
+	else if (c == 'p') // The void * pointer argument has to be printed in hexadecimal format
+		c = c;
+	else if (c == 'd') // Prints a decimal (base 10) number.
+		c = c;
+	else if (c == 'i') // Prints an integer in base 10.
+		c = c;
+	else if (c == 'u') // Prints an unsigned decimal (base 10) number.
+		c = c;
+	else if (c == 'x') // Prints a number in hexadecimal (base 16) lowercase format
+		c = c;
+	else if (c == 'X') // Prints a number in hexadecimal (base 16) uppercase format.
+		c = c;
+	else if (c == '%') // Prints a percent sign.
+		prn_cnt += ft_putchar('%');
+	else
+	{
+		prn_cnt += ft_putchar('%');
+		prn_cnt += ft_putchar(c);	
+	}	
+	return (prn_cnt);
+
+//cspdiuxX%
+
+}
+
+
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	ap;
 	int	i;
+	int	prn_cnt;
 
+	prn_cnt = 0;
 	i = 0;
 	va_start(ap, str);
 	while (str[i] != '\0')
@@ -29,34 +83,27 @@ int	ft_printf(const char *str, ...)
 		if (str[i] == '%')
 		{
 			i++;
-			if (str[i] == 'c')
-			{
-				ft_putchar(va_arg(ap, int));
-			}
-			else if (str[i] == '%')
-				ft_putchar('%');
-			else
-			{
-				i--;
-				ft_putchar(str[i]);
-			}
+			prn_cnt += pnt_decide(str[i], ap);			
 		}
-		else	
-			ft_putchar(str[i]);			
-		i++;
-	}
-	ft_putchar('\n');	
+		else
+		{
+			prn_cnt += ft_putchar(str[i]);
+		}
+
+		i++;		
+	}		
 	va_end(ap);
-	return (0);
+	return (prn_cnt);
 }
 
 #include <stdio.h>
 
 int	main(void)
 {
-	ft_printf("**%c***%d*****%c*****%c*******%a***%c****%*********%c", 'H', 'e', 'l', 'l', 'o');
+	printf("%d",ft_printf("*%c*%s%r rc%%=", 'A',"aa"));
 	printf("\n");
-	printf("%% %                             choj",'a');
+	printf("\n");
+	printf("%d",printf("*%c*%s%r rc%%=", 'A', "aa"));
 	printf("\n");
 	return(0);
 }
--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: jrosmari <jrosmari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 17:11:51 by jrosmari          #+#    #+#             */
-/*   Updated: 2023/01/27 18:07:42 by jrosmari         ###   ########.fr       */
+/*   Updated: 2023/01/28 10:25:01 by jrosmari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@
 
 typedef struct s_print                       
 {                        
-      va_list  args;    # arg to print out                    
-      int   wdt;        # width                
-      int   prc;        # precision              
-      int   zero;       # zero padding               
-      int   pnt;        # .     
-      int   dash;       # -               
-      int   tl;         # total_length (return value)              
-      int   sign;       # pos or neg number               
-      int   is_zero;    # is number zero                 
-      int   perc;       # %               
-      int   sp;         # space flag ' '            
+      va_list  args;    // arg to print out                    
+      int   wdt;        // width                
+      int   prc;        // precision              
+      int   zero;       // zero padding               
+      int   pnt;        // .     
+      int   dash;       // -               
+      int   tl;         // total_length (return value)              
+      int   sign;       // pos or neg number               
+      int   is_zero;    // is number zero                 
+      int   perc;       // %               
+      int   sp;         // space flag ' '            
 }    t_print;
 
 int	ft_printf(const char *, ...);
